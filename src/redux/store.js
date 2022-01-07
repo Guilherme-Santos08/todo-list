22ddb5859dddfd355f3dd2d46c2ac3840aedbad5
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-01-07 10:51:43 -0300

    feat: localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,27 @@
 import { combineReducers, createStore } from "redux";
 import { CardReducer } from "./reducers/cardReducer";
 
+// https://dev.to/link2twenty/react-redux-and-localstorage-2lih
+function saveToLocalStorage(state) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("collectionUsingRedux", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("collectionUsingRedux");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch {
+    return [];
+  }
+}
+
 const reducer = combineReducers({ collection: CardReducer });
-export const store = createStore(reducer);
+
+export const store = createStore(reducer, loadFromLocalStorage());
+store.subscribe(() => saveToLocalStorage(store.getState()));
